7dead5774c425b96847df0171fe0c7129b3103c8
Pedro N <itiropedro@gmail.com>
2026-03-17 20:08:48 -0300

    Adding go remote module so we get `go.sum`

--- qualify-backend/main.go
+++ qualify-backend/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/example/hello/reverse"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -12,5 +14,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 	fmt.Println("Server is running on port 8001")
+	fmt.Println(reverse.String("Hello"))
 	http.ListenAndServe(":8001", nil)
 }
