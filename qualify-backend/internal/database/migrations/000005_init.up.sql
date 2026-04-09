a4573475f326b43b764ccd2167108115280a6491
Pedro N <itiropedro@gmail.com>
2026-04-08 22:57:40 -0300

    Entrypoint fixes

--- qualify-backend/internal/database/migrations/000005_init.up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS user_skill (
-    user_id  int REFERENCES "user"(id) ON DELETE CASCADE,
-    skill_id int REFERENCES skill(id) ON DELETE CASCADE,
-    PRIMARY KEY (user_id, skill_id)
-);
