d809368e8cadb2c465f27ab43e11127e42f2b2b9
Pedro N <itiropedro@gmail.com>
2026-04-08 23:03:42 -0300

    squashing

--- qualify-backend/internal/database/migrations/000003_init.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" 
-DROP CONSTRAINT email_format;
--- qualify-backend/internal/database/migrations/000003_init.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user" 
-ADD CONSTRAINT email_format CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$');
