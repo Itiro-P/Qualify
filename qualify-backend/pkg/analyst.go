fe91de45f3c32827e0ab2ef3295827a5c1263110
Pedro N <itiropedro@gmail.com>
2026-04-10 09:55:14 -0300

    Remove redundant field

--- qualify-backend/pkg/analyst.go
+++ qualify-backend/pkg/analyst.go
@@ -2,7 +2,6 @@ package pkg
 
 type Analyst struct {
 	User
-	Timezone      string  `json:"timezone"`
 	Hourly_rate   float64 `json:"hourly_rate"`
 	Total_reviews int     `json:"total_reviews"`
 	Mean_rating   int     `json:"mean_rating"`
