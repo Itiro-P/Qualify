845b7c9083622e494114b10f94f67327d9167636
JoaoR090 <jrzr432@gmail.com>
2026-05-08 20:33:25 -0300

    vai

--- qualify-frontend/app/analyst/profile/page.tsx
+++ qualify-frontend/app/analyst/profile/page.tsx
@@ -1,3 +1,4 @@
+import { Footer, Header } from "@/components";
 import {
   ImageProfile,
   StatisticProfile,
@@ -6,22 +7,47 @@ import {
   ContactButtons,
 } from "@/components/analyst/profile";
 
-import { Footer, Header } from "@/components";
+import { getSessionUser } from "@/libs/session";
+import { useState, useEffect } from "react";
+import { Analyst } from "@/types/services";
+import { analystService } from "@/libs";
+import { useRouter } from "next/router";
 
 export default function Profile() {
-  return (
+  const router = useRouter();
+  const [analyst, setAnalyst] = useState<Analyst | null>(null);
+
+  useEffect(() => {
+    const session = getSessionUser();
+
+    if (!session) {
+      router.push("/User/register");
+      return;
+    }
+
+    analystService
+      .getByUserId(session.id)
+      .then((resp) => setAnalyst(resp.analyst));
+  }, [router]);
+
+  return analyst ? (
     <section id="profile" className="px-6 md:px-20 py-14">
       <Header />
+
       <div className="flex justify-between ml-3 mt-10">
         <ImageProfile />
         <Informations />
         <ContactButtons />
       </div>
+
       <div className="flex mt-10">
-        <About />
+        <About analyst={analyst} />
         <StatisticProfile />
       </div>
+
       <Footer />
     </section>
+  ) : (
+    <p>Usuário não logado ou não é analista</p>
   );
 }
