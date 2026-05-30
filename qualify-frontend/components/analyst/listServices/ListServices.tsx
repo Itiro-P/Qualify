08db5d84f459d06806c8c9d15a4d4a9051cd6b51
JoaoR090 <jrzr432@gmail.com>
2026-05-30 19:33:07 -0300

    commit-20260530-193307-26325

---
+++ qualify-frontend/components/analyst/listServices/ListServices.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Analyst } from "@/types/services/analyst";
+import { Service } from "@/types/services/service";
+import { useEffect, useState } from "react";
+
+function ListServicesArray({ services }: { services: Service[] }) {
+  return (
+    <div>
+      {services.map((service) => (
+        <div key={service.id}>
+          <p>{service.title}</p>
+          <p>{service.content}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export function ListServices({ analyst }: { analyst: Analyst }) {
+  const [servicesCompleteds, setServicesCompleteds] = useState<Service[]>([]);
+  const [servicesProposal, setServicesProposal] = useState<Service[]>([]);
+  const [servicesNegotiation, setServicesNegotiation] = useState<Service[]>([]);
+  const [servicesBlocked, setServicesBlocked] = useState<Service[]>([]);
+  const [servicesInProgress, setServicesInProgress] = useState<Service[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [abaAtiva, setAbaAtiva] = useState<
+    "propostas" | "negociacao" | "em andamento" | "bloqueados" | "concluidos"
+  >("concluidos");
+
+  useEffect(() => {
+    async function getInfo() {
+      // implementar forma de pegar os serviços do analista logado
+    }
+    getInfo();
+  }, [analyst.id]);
+
+  return (
+    <div>
+      <div className="flex">
+        <button
+          className="p-2 m-2 bg-blue-950"
+          onClick={() => setAbaAtiva("propostas")}
+        >
+          Propostas
+        </button>
+
+        <button
+          className="p-2 m-2 bg-blue-950"
+          onClick={() => setAbaAtiva("negociacao")}
+        >
+          Negociação
+        </button>
+
+        <button
+          className="p-2 m-2 bg-blue-950"
+          onClick={() => setAbaAtiva("bloqueados")}
+        >
+          Bloqueados
+        </button>
+
+        <button
+          className="p-2 m-2 bg-blue-950"
+          onClick={() => setAbaAtiva("em andamento")}
+        >
+          Em Andamento
+        </button>
+
+        <button
+          className="p-2 m-2 bg-blue-950"
+          onClick={() => setAbaAtiva("concluidos")}
+        >
+          Concluídos
+        </button>
+      </div>
+
+      <div className="mt-4">
+        {abaAtiva === "propostas" && (
+          <ListServicesArray services={servicesProposal} />
+        )}
+
+        {abaAtiva === "negociacao" && (
+          <ListServicesArray services={servicesNegotiation} />
+        )}
+
+        {abaAtiva === "em andamento" && (
+          <ListServicesArray services={servicesInProgress} />
+        )}
+
+        {abaAtiva === "bloqueados" && (
+          <ListServicesArray services={servicesBlocked} />
+        )}
+
+        {abaAtiva === "concluidos" && (
+          <ListServicesArray services={servicesCompleteds} />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ qualify-frontend/components/analyst/listServices/index.ts
@@ -0,0 +1,6 @@
+export { ListServices } from "./ListServices";
+export { Blocked } from "./Blocked";
+export { InProgress } from "./InProgress";
+export { Negotiation } from "./Negotiation";
+export { Proposals } from "./Proposals";
+export { Completeds } from "./Completeds";
