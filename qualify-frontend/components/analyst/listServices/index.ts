484fa11869e4a196a444274f8a55bf4bb430bf6c
JoaoR090 <jrzr432@gmail.com>
2026-05-30 19:54:51 -0300

    commit-20260530-195451-7674

--- qualify-frontend/components/analyst/listServices/index.ts
+++ qualify-frontend/components/analyst/listServices/index.ts
@@ -1,6 +1,1 @@
 export { ListServices } from "./ListServices";
-export { Blocked } from "./Blocked";
-export { InProgress } from "./InProgress";
-export { Negotiation } from "./Negotiation";
-export { Proposals } from "./Proposals";
-export { Completeds } from "./Completeds";
