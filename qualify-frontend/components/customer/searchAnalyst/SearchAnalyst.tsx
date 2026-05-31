3891ce80d2a775ddcbcd7d5ac49a370c44a60e40
JoaoR090 <jrzr432@gmail.com>
2026-05-31 12:10:40 -0300

    commit-20260531-121039-24457

--- qualify-frontend/components/customer/searchAnalyst/SearchAnalyst.tsx
+++ qualify-frontend/components/customer/searchAnalyst/SearchAnalyst.tsx
@@ -5,10 +5,23 @@ import { Loading } from "@/components/ui/Loading";
 import { Analyst } from "@/types/services/analyst";
 import { useState } from "react";
 
+interface Location {
+  country: string;
+  state: string;
+  city: string;
+}
+
+interface IFormResponse {
+  value: number;
+  skills: string[];
+  localization: Location;
+}
+
 export function SearchAnalyst() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [analysts, setAnalysts] = useState<Analyst[]>([]);
+  const [formResponse, setFormResponse] = useState<IFormResponse | null>(null);
 
   return loading ? (
     <Loading />
@@ -16,21 +29,103 @@ export function SearchAnalyst() {
     <div>
       {error && <Alert variant="error">{error}</Alert>}
       <form>
-        <h3>Categorias</h3>
-        <FormInput label="Eletrônicos" type="checkbox" />
-        <FormInput label="Informática" type="checkbox" />
-        <FormInput label="Celulares" type="checkbox" />
-        <FormInput label="Casa" type="checkbox" />
-        <h3>Preço</h3>
-        <FormInput label="Preço" type="range" min="0" max="5000" value="2500" />
-        <h3>Avaliação</h3>
-        <FormInput label="★★★★★" type="checkbox" />
-        <FormInput label="★★★★☆ ou mais" type="checkbox" />
-        <FormInput label="★★★☆☆ ou mais" type="checkbox" />
-        <h3>Entrega</h3>
-        <FormInput label="Frete grátis" type="checkbox" />
-        <FormInput label="Full" type="checkbox" />
-        <FormInput label="Entrega rápida" type="checkbox" />
+        <h3>Habilidades</h3>
+        <FormInput
+          label="Habilidades"
+          onChange={(e) =>
+            setFormResponse({
+              value: formResponse?.value || 0,
+              skills: formResponse
+                ? [...formResponse.skills, e.target.value]
+                : [e.target.value],
+              localization: formResponse?.localization || {
+                country: "",
+                state: "",
+                city: "",
+              },
+            })
+          }
+        />
+        <h3>Preço por hora</h3>
+        <FormInput
+          label="Preço"
+          type="range"
+          min="0"
+          max="5000"
+          value={formResponse?.value || 0}
+          onChange={(e) =>
+            setFormResponse({
+              value: Number(e.target.value),
+              skills: formResponse?.skills || [],
+              localization: formResponse?.localization || {
+                country: "",
+                state: "",
+                city: "",
+              },
+            })
+          }
+        />
+        <h3>Localização</h3>
+        <FormInput
+          label="País"
+          value={formResponse?.localization?.country || ""}
+          onChange={(e) =>
+            setFormResponse({
+              value: formResponse?.value || 0,
+              skills: formResponse?.skills || [],
+              localization: formResponse?.localization
+                ? {
+                    ...formResponse.localization,
+                    country: e.target.value,
+                  }
+                : {
+                    country: e.target.value,
+                    state: "",
+                    city: "",
+                  },
+            })
+          }
+        />
+        <FormInput
+          label="Estado"
+          value={formResponse?.localization?.state || ""}
+          onChange={(e) =>
+            setFormResponse({
+              value: formResponse?.value || 0,
+              skills: formResponse?.skills || [],
+              localization: formResponse?.localization
+                ? {
+                    ...formResponse.localization,
+                    state: e.target.value,
+                  }
+                : {
+                    country: "",
+                    state: e.target.value,
+                    city: "",
+                  },
+            })
+          }
+        />
+        <FormInput
+          label="Cidade"
+          value={formResponse?.localization?.city || ""}
+          onChange={(e) =>
+            setFormResponse({
+              value: formResponse?.value || 0,
+              skills: formResponse?.skills || [],
+              localization: formResponse?.localization
+                ? {
+                    ...formResponse.localization,
+                    city: e.target.value,
+                  }
+                : {
+                    country: "",
+                    state: "",
+                    city: e.target.value,
+                  },
+            })
+          }
+        />
       </form>
     </div>
   );
